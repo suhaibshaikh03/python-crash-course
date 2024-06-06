112e57fda9b770062682b867edceee72f5583db7
Suhaib Shaikh <suhaibshaikh2003@gmail.com>
2024-06-06 07:57:54 -0700

    Add files via upload

---
+++ class1/class1.py
@@ -0,0 +1,15 @@
+#printing syntax
+#print("Pakistan Zindabad")
+#name="suhaib"
+#print(name)
+#print(type(name))
+#print(id(name))
+#print(dir(name))
+
+#name:str = 500 #error
+#print(name)
+name:list[str] = ['a','b','c','d']
+print(name)
+print(type(name))#datatype
+print(id(name))#datatype
+print(dir(name))#methods and attributes
---
+++ class1/readme.md
@@ -0,0 +1,9 @@
+Installation
+*Anaconda
+*vscode
+*python extension by microsoft
+
+conda create -n xyz python==3.12 -y(for creating environment)
+conda activate xyz(for activation)
+conda deactivate(for deactivation)
+pip install -r requirement.txt
